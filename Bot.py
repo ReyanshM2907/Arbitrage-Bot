7517928c7296fed3174d99ffb845abbee3647f10
Reyansh M <98943084+ReyanshM2907@users.noreply.github.com>
2022-10-31 18:36:56 +0530

    Change to Goerli Test network

--- Bot.py
+++ Bot.py
@@ -1,6 +1,6 @@
 from web3 import Web3
 
-infura_url = "https://mainnet.infura.io/v3/97b8b242908c4d1591210f7dcaa76c64"
+infura_url = "https://goerli.infura.io/v3/97b8b242908c4d1591210f7dcaa76c64"
 web3 = Web3(Web3.HTTPProvider(infura_url))
 
 print(web3.isConnected())
